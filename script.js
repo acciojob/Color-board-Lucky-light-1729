98bb857d5638df8da65d5dc5687b396a0b39d1a3
Anay Agrawal <agrawalanay20@gmail.com>
2025-06-19 13:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const container = document.querySelector('.container');
+const SQUARES = 800;
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseenter', () => {
+    setColor(square);
+    setTimeout(() => removeColor(square), 1000); // remove after 1s
+  });
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = 'transparent';
+}
+
+function getRandomColor() {
+  const colors = ['#e74c3c', '#8e44ad', '#3498db', '#2ecc71', '#f1c40f', '#e67e22'];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
